a4889b30bb57e30228acc0e961f2ae71d64e72de
Serhii <89202728+serhiihonta@users.noreply.github.com>
2023-07-28 19:50:20 +0300

    hw 4

--- Homework 4/homework04.py
+++ Homework 4/homework04.py
@@ -10,3 +10,70 @@
 для випадковості значень скористайтесь модулем random. приклад згенерованого словника:
 {'name':'Liza', 'surname':'Djoconda', 'location':'Florence'}
 """
+
+"""Task 1"""
+
+firstList = [1, 2, 3, 4, 5]
+secondList = [4, 5, 6, 7, 8, 1]
+
+
+def finddistinct(a, b):
+    """Find common elements in lists"""
+    setone = set(a)
+    settwo = set(b)
+    common = sorted(setone.intersection(settwo))
+    print(common)
+
+
+finddistinct(firstList, secondList)
+
+"""Task 2"""
+
+students = {'Oksana': 15, 'Kolya': 22, 'Dima': 6, 'Natasha': 11}
+
+
+def average(dict):
+    """Find average"""
+    total_sum = sum(dict.values())
+    total_len = len(dict)
+    average = total_sum / total_len
+    return average
+
+
+def above_average(dict, average):
+    """Create a list above average values"""
+    above_avg_students = []
+    for name, score in dict.items():
+        if score > average:
+            above_avg_students.append((name, score))
+    return above_avg_students
+
+
+avg_score = average(students)
+above_avg = above_average(students, avg_score)
+print(f"The average score is {avg_score}. Students with score above average: {above_avg}")
+
+"""Task 3"""
+
+import random
+
+name_list = ['Oksana', 'Kolya', 'Igor', 'Natasha']
+surname_list = ['Nikolaev', 'Oksanova', 'Igorev', 'Natashich']
+locations_list = ['Chernihiv', 'Nizhin', 'Priluky', 'Snovsk']
+
+
+def create_random_people():
+    """Create a random set of personal data from lists"""
+    random_name = random.choice(name_list)
+    random_surname = random.choice(surname_list)
+    random_location = random.choice(locations_list)
+
+    person = {'name': random_name, 'surname': random_surname, 'location': random_location}
+    return person
+
+
+number_people = 6
+
+for i in range(number_people):
+    random_people = create_random_people()
+    print(random_people)
